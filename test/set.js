865dbdd981ac4bf87784eb37df03def7d2be9455
lukasz_brainhub <lukasz@brainhub.pl>
2017-12-14 23:18:33 +0100

    Updated readme, added splice function, added support for array-like syntax in path in set function

--- test/set.js
+++ test/set.js
@@ -51,19 +51,103 @@ describe('set', () => {
       unmodifiedField: { field: 3 },
     });
   });
-  it('should set value in nested path with custom path delimiter', () => {
-    const obj1 = { field: { subField: 1 }, unmodifiedField: { field: 3 } };
 
-    const obj2 = set(obj1, 'field/subField', 2, '/');
+  it('should set a value in array without mutating it', () => {
+    const arr1 = [{ foo: 'one' }, { foo: 'two' }, { foo: 'four' }];
 
-    expect(obj2, 'Newly created object equals source object. It has probably been mutated').to.not.equal(obj1);
-    expect(obj2.field, 'Intermediate object in path equals source. It has probably been mutated')
-      .to.not.equal(obj1.field);
-    expect(obj2.unmodifiedField, 'Unmodified nested object has been unnecessarily replaced with new one')
-      .to.equal(obj1.unmodifiedField);
-    expect(obj2).to.deep.equal({
-      field: { subField: 2 },
-      unmodifiedField: { field: 3 },
+    const arr2 = set(arr1, '[2]', { foo: 'three' });
+
+    expect(arr2, 'Newly created array equals source array. It has probably been mutated').to.not.equal(arr1);
+    expect(arr2[0], 'Unmodified nested object has been unnecessarily replaced with new one')
+      .to.equal(arr1[0]);
+    expect(arr2).to.deep.equal([
+      { foo: 'one' },
+      { foo: 'two' },
+      { foo: 'three' },
+    ]);
+  });
+  it('should set value in nested array without mutating any of the nested objects and arrays', () => {
+    const arr1 = [
+      [
+        { foo: 'one' },
+        { bar: 'one' },
+      ],
+      [
+        { foo: 'two' },
+        { bar: 'two' },
+      ],
+      { foo: 'four' },
+    ];
+
+    const arr2 = set(arr1, '[1][1].bar', 'three');
+
+    expect(arr2, 'Newly created array equals source array. It has probably been mutated').to.not.equal(arr1);
+
+    expect(arr2[1], 'Intermediate object in path equals source. It has probably been mutated')
+      .to.not.equal(arr1[1]);
+    expect(arr2[1][1], 'Intermediate object in path equals source. It has probably been mutated')
+      .to.not.equal(arr1[1][1]);
+
+    expect(arr2[0], 'Unmodified nested object has been unnecessarily replaced with new one')
+      .to.equal(arr2[0]);
+
+    expect(arr2).to.deep.equal([
+      [
+        { foo: 'one' },
+        { bar: 'one' },
+      ],
+      [
+        { foo: 'two' },
+        { bar: 'three' },
+      ],
+      { foo: 'four' },
+    ]);
+  });
+  it('should set a collection of nested arrays', () => {
+    const arr1 = [
+      [
+        { foo: 'one' },
+        { bar: 'one' },
+      ],
+      [
+        { foo: 'two' },
+        { bar: 'two' },
+      ],
+      { foo: 'four' },
+    ];
+
+    const arr2 = set(arr1, {
+      '[1][1].bar': 'three',
+      '[1][0].foo': 'four',
     });
+
+    expect(arr2, 'Newly created array equals source array. It has probably been mutated').to.not.equal(arr1);
+
+    expect(arr2[1], 'Intermediate object in path equals source. It has probably been mutated')
+      .to.not.equal(arr1[1]);
+    expect(arr2[1][1], 'Intermediate object in path equals source. It has probably been mutated')
+      .to.not.equal(arr1[1][1]);
+
+    expect(arr2[0], 'Unmodified nested object has been unnecessarily replaced with new one')
+      .to.equal(arr2[0]);
+
+    expect(arr2).to.deep.equal([
+      [
+        { foo: 'one' },
+        { bar: 'one' },
+      ],
+      [
+        { foo: 'four' },
+        { bar: 'three' },
+      ],
+      { foo: 'four' },
+    ]);
+  });
+
+  it('should throw an error when provided with unsupported value as a first argument (not object/array)', () => {
+    expect(() => set(2, '[2]', 'value')).to.throw(Error);
+  });
+  it('should throw an error when provided with unsupported value as a second argument (not object/array/string)', () => {
+    expect(() => set({ foo: 'bar' }, 2, 'value')).to.throw(Error);
   });
 });
