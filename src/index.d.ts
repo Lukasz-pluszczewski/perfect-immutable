e68771f5462c69fb8b82ac83a58ccfc273e0efbb
Daniel Chmielowiec <daniel.chmielowiec@jazzy.pro>
2018-01-23 14:17:04 +0100

    WIP: Type definitions. Lacks: fp, filter. Needs improovement: set

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,23 @@
 declare module 'perfect-immutable' {
+    type ImmutableSetReturnFromPathString<V> = {
+        [x: string]: V
+    };
+    type ImmutableSetReturnFromPathObject<P> = {
+        [K in keyof P]: P[K];
+    }
+    interface ImmutableSetTransformFunction<T> {
+        (item: T): T
+    }
+
+    interface ImmutableSet {
+        <T>(target: T[], path: number, value: T, setFunction?: boolean): T[]
+        <T>(target: T[], path: number, value: ImmutableSetTransformFunction<T>, setFunction?: boolean): T[]
+
+        <T, V>(target: T, path: string, value: V, setFunction?: boolean): T & ImmutableSetReturnFromPathString<V>
+        <T, V>(target: T, path: string, value: ImmutableSetTransformFunction<T, V>, setFunction?: boolean): T & ImmutableSetReturnFromPathString<V>
+
+        <T, P>(target: T, path: P, value?: any, setFunction?: boolean): T & ImmutableSetReturnFromPathObject<P>
+    }
     interface ImmutableSplice {
         <T>(arr: T[], start: number, deleteCount: number, ...items: T[]): T[];
     }
@@ -25,6 +44,7 @@ declare module 'perfect-immutable' {
         <T>(obj: T, key: keyof T): Partial<T>;
     }
 
+    export const set: ImmutableSet;
     export const splice: ImmutableSplice;
     export const pop: ImmutablePop;
     export const push: ImmutablePush;
